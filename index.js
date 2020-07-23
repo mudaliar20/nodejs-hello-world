75d866d267417c12ebbb17f00159f7e4598e1384
mudaliar20 <54627121+mudaliar20@users.noreply.github.com>
2020-07-23 19:52:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-var http = require('http');
-
-var server = http.createServer(function(request, response) {
-
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
-
-});
-
-var port = 80;
-server.listen(port);
-
-console.log("Server running at http://localhost:%d", port);
+exports.handler = function (event, context) {
+    context.succeed('hello world');
+};
